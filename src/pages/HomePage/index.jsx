c8e5c4ad2bc54b3b3f69b4dbab0b6bff7a59cd74
Viktoriia Nykyforova <113258083+ViktoriiaKN@users.noreply.github.com>
2024-05-25 11:03:25 +0200

    Router,  edits HomePage, App

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,7 +1,19 @@
-import './style.css';
+import { useState } from 'react';
+import { Rooms } from '../../components/Rooms';
+
 
 export const HomePage = () => {
+
+ /*  const [journey, setJourney] = useState(null);
+
+  function handleJourneyChange (journeyData) {
+    setJourney(journeyData);
+    }
+ */
   return (
-    
+    <main>
+      <Rooms/>
+      <RoomDetail/>
+    </main>
   );
 };
