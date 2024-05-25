711230805169e591b6a419730ae06234639a7f76
Viktoriia Nykyforova <113258083+ViktoriiaKN@users.noreply.github.com>
2024-05-25 14:03:05 +0200

    commit Registration

--- src/components/RoomDetails/index.jsx
+++ src/components/RoomDetails/index.jsx
@@ -1,8 +1,22 @@
+import { Registration } from "../Registration";
+
 export const RoomDetails = () => {
   return (
-    <section>
-      <h2>Room Details</h2>
-      <p>Room details go here...</p>
-    </section>
+    <section class="light">
+    <div class="container">
+      <h2>Heading</h2>
+      <div class="columns-2">
+        <div class="column">
+          <img src="img/image1.svg" />
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque accusantium, dolor quisquam doloremque quod nobis temporibus ducimus sapiente consectetur distinctio assumenda, nisi suscipit saepe. Vero.
+          </p>
+        </div>
+        
+      </div>
+    </div>
+    <Registration/>
+  </section>
+
   );
 } 
